8a311a858463506320962d2b8929859b517fa044
Bisma Ayaz <bisma.ayaz@yahoo.com>
2016-06-23 17:08:27 +0500

    Add files via upload

--- reset.php
+++ reset.php
@@ -1,79 +1,4 @@
-<script type="text/javascript">
-
-  document.addEventListener("DOMContentLoaded", function() {
-
-    // JavaScript form validation
-
-    var checkPassword = function(str)
-    {
-      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-      return re.test(str);
-    };
-
-    var checkForm = function(e)
-    {
-      
-      if(this.pwd1.value != "" && this.pwd1.value == this.pwd2.value) {
-        if(!checkPassword(this.pwd1.value)) {
-          alert("The password you have entered is not valid!");
-          this.pwd1.focus();
-          e.preventDefault();
-          return;
-        }
-      } else {
-        alert("Error: Please check that you've entered and confirmed your password!");
-        this.pwd1.focus();
-        e.preventDefault();
-        return;
-      }
-    };
-
-    var myForm = document.getElementById("myForm");
-    myForm.addEventListener("submit", checkForm, true);
-
-    // HTML5 form validation
-
-    var supports_input_validity = function()
-    {
-      var i = document.createElement("input");
-      return "setCustomValidity" in i;
-    }
-
-    if(supports_input_validity()) {
-      var usernameInput = document.getElementById("field_username");
-      usernameInput.setCustomValidity(usernameInput.title);
-
-      var pwd1Input = document.getElementById("field_pwd1");
-      pwd1Input.setCustomValidity(pwd1Input.title);
-
-      var pwd2Input = document.getElementById("field_pwd2");
-
-      // input key handlers
-
-      usernameInput.addEventListener("keyup", function() {
-        usernameInput.setCustomValidity(this.validity.patternMismatch ? usernameInput.title : "");
-      }, false);
-
-      pwd1Input.addEventListener("keyup", function() {
-        this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
-        if(this.checkValidity()) {
-          pwd2Input.pattern = this.value;
-          pwd2Input.setCustomValidity(pwd2Input.title);
-        } else {
-          pwd2Input.pattern = this.pattern;
-          pwd2Input.setCustomValidity("");
-        }
-      }, false);
-
-      pwd2Input.addEventListener("keyup", function() {
-        this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
-      }, false);
-
-    }
-
-  }, false);
-  </script>
-  <?php
+<?php
 require_once('config.php');
 if (isset($_GET['email']) && preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/', $_GET['email']))
 {
@@ -83,17 +8,25 @@ if (isset($_GET['key']) && (strlen($_GET['key']) == 32))//The Activation key wil
 {
     $key = $_GET['key'];
 }
+
 $user_query = mysqli_query($con,"SELECT active_time FROM candidate WHERE cand_email= '$email' ")or die(mysqli_error($con));
-													while($row = mysqli_fetch_array($user_query)){
+													$row = mysqli_fetch_array($user_query);
 													$time = $row['active_time'];
 													$timestamp = strtotime('$time') + 60*60*60;
 													$time = date('H:i', $timestamp);
-													if($time > NOW()) {
+													if($time > date("h:i:sa")) {
 if (isset($_POST['update']))
 { 
-$npassword= $_POST['pwd1'];
-  $result = mysqli_query($con,"UPDATE candidate SET Activation=NULL,cand_password = '$npassword' WHERE cand_email ='$email' LIMIT 1") or die(mysqli_error($con));
-
+$npassword= md5(mysql_real_escape_string($_POST['pwd1']));
+if ($npassword=="" || $email == "")
+{ ?>
+<script>
+alert('Email not sent');
+window.location = "index.php";
+</script>
+<?php exit(); }
+else {
+  $result = mysqli_query($con,"UPDATE candidate SET Activation=NULL,cand_password = '$npassword' WHERE cand_email ='$email'") or die(mysqli_error($con));
 
     // Print a customized message:
     if ($result)//if update query was successfull
@@ -105,7 +38,7 @@ $npassword= $_POST['pwd1'];
         echo '<div class="errormsgbox">Oops !Your account could not be activated. Please recheck the link or contact the system administrator.</div>';
 
     }
-	}}
+	}}}
 	else { 
 ?>
 <script>
@@ -115,24 +48,19 @@ window.location = "index.php";
 <?php } ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="js/ie-emulation-modes-warning.js"></script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	
@@ -144,82 +72,86 @@ window.location = "index.php";
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
 	<link href="css/alert.css" rel="stylesheet">
-
+<link href="css/error.css" rel="stylesheet">
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
   <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
   <![endif]-->
-  
 <link rel="shortcut icon" href="assets/img/logocalc1.png">
-
-	
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/scripts.js"></script>
-    <title>Edit User</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-  
-
-
-    <!-- Custom Fonts -->
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+<title>Edit User</title>
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->	 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+    <![endif]-->
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-  <body bgcolor="#FFFFFF">
- <form class="form-horizontal" role="form" method="post" autocomplete="off">
-			<h3><center>Recover your account</center></h3>
-			<br>									
-		      
-           
-            <div class="form-group">
-              <label class="col-md-5 control-label" for="rental">New:</label>
-              <div class="col-md-3">
-              <input id="field_pwd1" title="Password must contain at least 6 characters, including UPPER/lowercase and numbers." class="form-control input-md" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" name="pwd1">
-              </div>
-            </div>
-             <div class="form-group">
-              <label class="col-md-5 control-label" for="rental">Re-type New:</label>
-              <div class="col-md-3">
-             <input id="field_pwd2" title="Please enter the same Password as above." type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" class="form-control input-md" name="pwd2">
-              </div>
-            </div>
-               <div class="control-group">
-				<div class="controls" align="center">
-						           				 <button name="update" class="btn btn-success">Save Changes</button>
-												 <a button id="cancel" name="cancel" class="btn btn-danger" href="index.php" >Cancel</button></a>
-						           			 </div></div>
-											 
-									</center>
-                                 </form>      
-                                    
-                          
-<!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
+<body bgcolor="#FFFFFF">
+<form class="form-horizontal" role="form" method="post" autocomplete="off">
+  <h3>
+    <center>
+      Recover your account
+    </center>
+  </h3>
+  <br>
+  <div class="form-group">
+    <label class="col-md-5 control-label" for="rental">New:</label>
+    <div class="col-md-3">
+      <input type="password" placeholder="Password" id="password" class="form-control input-md" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" name="pwd1" 
+                title="Password should contain an upper case letter, a lower case letter, a number and a special character. Length should be atleast 8 characters" name="password" 
+required />
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="col-md-5 control-label" for="rental">Re-type New:</label>
+    <div class="col-md-3">
+      <input type="password" placeholder="Confirm Password" id="confirm_password" class="form-control input-md" name="pwd2" required>
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls" align="center">
+      <button name="update" class="btn btn-success">Save Changes</button>
+      <a button id="cancel" name="cancel" class="btn btn-danger" href="index.php" >Cancel
+      </button>
+      </a> </div>
+  </div>
+  </center>
+</form>
+<script>
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+   
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Password Doesn't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+</script>
+<script src="js/jquery-1.11.0.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Morris Charts JavaScript -->
+<script src="js/plugins/morris/raphael.min.js"></script>
+<script src="js/plugins/morris/morris.min.js"></script>
+<script src="js/plugins/morris/morris-data.js"></script>
 </body>
 </html>
