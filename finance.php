f6130f80e0c75456ca1b05c37a5c4d67f8796dfb
Bisma Ayaz <bisma.ayaz@yahoo.com>
2016-06-23 19:56:49 +0500

    Add files via upload

--- finance.php
+++ finance.php
@@ -5,32 +5,66 @@ require_once('config.php');
 require_once('session2.php');
 ?>
 <?php include('header.php'); ?>
-            <nav>
-  <div id="page-wrapper">
+
+<body>
+	 <div id="page-wrapper">
             <div class="container-fluid">	
   <nav>
    <div id="page-wrapper" class="page-wrapper-cls">
             <div id="page-inner">
                 <div class="row">
-		<div class="col-md-12">
-			<form class="form-horizontal" role="form" method="post">
-			<h3><center>Home</center></h3>
-			
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">DASHBOARD</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-info">
+                
+                        </div>
+                    </div>
+                </div>
+                           <div class="row">
+            <div class=" col-md-12 col-sm-12">
+               
+              
+             
+              <div class=" col-md-12 col-sm-12">
+                  <div class="list-group">
+                            <a href="#" class="list-group-item active">
+                                <h4 class="list-group-item-heading">You are logged in as: </h4>
+                                <p class="list-group-item-text" style="line-height: 30px;">
+                              <b> Email: <?php echo $_SESSION['email']; ?>
+                               <br>
+                             Total Voucher Inserted: <?php $zero=0;
+							
+							  $user_query = mysqli_query($con,"SELECT voucher_id FROM voucher WHERE admin_id=1 ")or die(mysqli_error($con));
+													$balance = mysqli_num_rows($user_query);
+													if($balance=="" || $balance==0)
+													echo $zero;
+													else echo $balance; ?></b>
+                                                    
+                              
+                                </p>
+                            </a>
+                        </div>
+                  <br />
+                  <div class=" col-md-12 col-sm-8">
+              <div id="calendar"></div>
+        </div>
+                               </div>
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+        </div>
+    <!-- /. WRAPPER  -->
+
   </nav>
   
   </div>
   </div>
   </div>
-    <!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
-	<?php include('script.php'); ?>
 </body>
 </html>
