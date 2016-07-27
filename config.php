4bc8d57e209e02b336caffb65ea197fedcbe2945
asifsadiq <asif.sadiq@live.com>
2016-07-27 11:55:50 +0500

    Add files via upload
    config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$database_name="pia";
+$database_host="localhost";
+$database_password="";
+$database_user="root";
+$con = mysqli_connect($database_host, $database_user, $database_password,$database_name); 
+define('GUSER', 'xxxxxx@gmail.com'); // GMail username
+define('GPWD', 'xxxxxx'); // GMail password
+define('WEBSITE_URL', 'http://localhost');
+?>
